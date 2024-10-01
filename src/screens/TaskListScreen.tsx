b124454435f8821482d29dfbabfab90f5ceb65b4
waritthorn-v <waritthorn.dee@gmail.com>
2024-10-02 00:33:12 +0700

    Implement delete feature

--- src/screens/TaskListScreen.tsx
+++ src/screens/TaskListScreen.tsx
@@ -1,12 +1,14 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import {
   View,
   Text,
-  FlatList,
   TouchableOpacity,
   StyleSheet,
   Button,
+  TouchableHighlight,
+  ListRenderItemInfo,
 } from 'react-native';
+import {SwipeListView} from 'react-native-swipe-list-view';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {DEFAULT_TASKS, EMPTY_TASK, generateUUID, Task} from '../utils/task';
 
@@ -27,7 +29,7 @@ interface TaskListScreenProps {
 const TaskListScreen: React.FC<TaskListScreenProps> = ({navigation}) => {
   const [tasks, setTasks] = React.useState<Task[]>(DEFAULT_TASKS);
 
-  const updateTask = useCallback((updatedTask: Task) => {
+  const updateTask = (updatedTask: Task) => {
     setTasks(prevTasks => {
       if (updatedTask.id) {
         return prevTasks.map(task =>
@@ -37,7 +39,11 @@ const TaskListScreen: React.FC<TaskListScreenProps> = ({navigation}) => {
       // add new task to the list
       return [...prevTasks, {...updatedTask, id: generateUUID()}];
     });
-  }, []);
+  };
+
+  const deleteTask = (index: number) => {
+    setTasks(prevTasks => prevTasks.filter((_, i) => i !== index));
+  };
 
   React.useEffect(() => {
     navigation.setOptions({
@@ -54,26 +60,43 @@ const TaskListScreen: React.FC<TaskListScreenProps> = ({navigation}) => {
         />
       ),
     });
-  }, [navigation, updateTask]);
+  }, [navigation]);
+
+  const renderHiddenItem = (data: ListRenderItemInfo<Task>, _: any) => {
+    const index = data.index;
+    return (
+      <View style={styles.hiddenContainer}>
+        <TouchableOpacity
+          style={styles.hiddenButton}
+          onPress={() => deleteTask(index)}>
+          <Text style={styles.backTextWhite}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   const renderTask = ({item}: {item: Task}) => (
-    <TouchableOpacity
+    <TouchableHighlight
+      underlayColor="white"
+      style={styles.taskContainer}
       onPress={() =>
         navigation.navigate('TaskDetail', {task: item, updateTask})
       }>
-      <View style={styles.taskContainer}>
+      <View style={styles.taskViewContainer}>
         <Text style={styles.taskTitle}>{item.title}</Text>
         <Text>Description: {item.description}</Text>
         <Text>Status: {item.status}</Text>
       </View>
-    </TouchableOpacity>
+    </TouchableHighlight>
   );
 
   return (
     <View style={styles.container}>
-      <FlatList
+      <SwipeListView
         data={tasks}
         renderItem={renderTask}
+        renderHiddenItem={renderHiddenItem}
+        rightOpenValue={-75}
         keyExtractor={(_, index) => index.toString()}
       />
     </View>
@@ -85,11 +108,32 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   taskContainer: {
+    backgroundColor: 'white',
+  },
+  taskViewContainer: {
     padding: 20,
     borderBottomWidth: 1,
   },
   taskTitle: {
     fontSize: 18,
+  },
+  // style for the hidden component
+  hiddenContainer: {
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  hiddenButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 75,
+    height: '100%',
+    backgroundColor: 'red',
+  },
+  backTextWhite: {
+    color: '#FFF',
   },
 });
 
